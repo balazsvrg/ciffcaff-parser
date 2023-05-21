8efa599a0f11a48a91738cd02b9b08896153a071
Balázs Varga <balazs.vrg98@gmail.com>
2023-05-21 16:41:59 +0000

    move constants to top

--- src/caff.cpp
+++ src/caff.cpp
@@ -3,7 +3,23 @@
 #include <iostream>
 #include <stdexcept>
 
-// CAFF class constructor
+const std::string VALID_MAGIC_VALUE = "CAFF";
+
+const uint32_t BLOCK_ID_LENGTH = 1;
+const uint32_t BLOCK_SIZE_LENGTH = 8;
+const uint32_t MAGIC_LENGTH = 4;
+const uint32_t HEADER_SIZE_LENGTH = 8;
+const uint32_t NUM_ANIM_LENGTH = 8;
+
+const uint32_t YEAR_LENGTH = 2;
+const uint32_t MONTH_LENGTH = 1;
+const uint32_t DAY_LENGTH = 1;
+const uint32_t HOUR_LENGTH = 1;
+const uint32_t MINUTE_LENGTH = 1;
+const uint32_t CREATOR_LENGTH = 8;
+
+const uint32_t DURATION_LENGTH = 8;
+
 CAFF::CAFF(const std::string& filename) : filename(filename) { }
 
 // Function to parse the CAFF image
@@ -20,24 +36,30 @@ bool CAFF::parse_image() {
         //read header block 
         uint8_t header_block_id;
         uint64_t header_block_size;
-        file.read(reinterpret_cast<char*>(&header_block_id), 1);
-        file.read(reinterpret_cast<char*>(&header_block_size), 8);
+        file.read(reinterpret_cast<char*>(&header_block_id), BLOCK_ID_LENGTH);
+        file.read(reinterpret_cast<char*>(&header_block_size), BLOCK_SIZE_LENGTH);
         header.magic.resize(4);
-        file.read(reinterpret_cast<char*>(&header.magic[0]), 4);
-        file.read(reinterpret_cast<char*>(&header.headerSize), sizeof(uint64_t));
-        file.read(reinterpret_cast<char*>(&header.numAnim), sizeof(uint64_t));
+        file.read(reinterpret_cast<char*>(&header.magic[0]), MAGIC_LENGTH);
+
+        if (header.magic.compare(VALID_MAGIC_VALUE) != 0){
+            throw std::invalid_argument("Magic value is not \"CAFF\"");
+        }
+
+
+        file.read(reinterpret_cast<char*>(&header.headerSize), HEADER_SIZE_LENGTH);
+        file.read(reinterpret_cast<char*>(&header.numAnim), NUM_ANIM_LENGTH);
 
         // Read CAFF credits block 
         uint8_t credits_block_id;
         uint64_t credits_block_size;
-        file.read(reinterpret_cast<char*>(&credits_block_id), 1);
-        file.read(reinterpret_cast<char*>(&credits_block_size), 8);
-        file.read(reinterpret_cast<char*>(&credits.year), sizeof(uint16_t));
-        file.read(reinterpret_cast<char*>(&credits.month), sizeof(uint8_t));
-        file.read(reinterpret_cast<char*>(&credits.day), sizeof(uint8_t));
-        file.read(reinterpret_cast<char*>(&credits.hour), sizeof(uint8_t));
-        file.read(reinterpret_cast<char*>(&credits.minute), sizeof(uint8_t));
-        file.read(reinterpret_cast<char*>(&credits.creatorLen), sizeof(uint64_t));
+        file.read(reinterpret_cast<char*>(&credits_block_id), BLOCK_ID_LENGTH);
+        file.read(reinterpret_cast<char*>(&credits_block_size), BLOCK_SIZE_LENGTH);
+        file.read(reinterpret_cast<char*>(&credits.year), YEAR_LENGTH);
+        file.read(reinterpret_cast<char*>(&credits.month), MONTH_LENGTH);
+        file.read(reinterpret_cast<char*>(&credits.day), DAY_LENGTH);
+        file.read(reinterpret_cast<char*>(&credits.hour), HOUR_LENGTH);
+        file.read(reinterpret_cast<char*>(&credits.minute), MINUTE_LENGTH);
+        file.read(reinterpret_cast<char*>(&credits.creatorLen), CREATOR_LENGTH);
         if (credits.creatorLen > 0) {
             credits.creator.resize(credits.creatorLen);
             file.read(&credits.creator[0], credits.creatorLen);
@@ -48,10 +70,10 @@ bool CAFF::parse_image() {
         for (uint64_t i = 0; i < header.numAnim; ++i) {
             uint8_t anim_block_id;
             uint64_t anim_block_size;
-            file.read(reinterpret_cast<char*>(&anim_block_id), 1);
-            file.read(reinterpret_cast<char*>(&anim_block_size), 8);
+            file.read(reinterpret_cast<char*>(&anim_block_id), BLOCK_ID_LENGTH);
+            file.read(reinterpret_cast<char*>(&anim_block_size), BLOCK_SIZE_LENGTH);
             CaffAnimation& animation = animations[i];
-            file.read(reinterpret_cast<char*>(&animation.duration), sizeof(uint64_t));
+            file.read(reinterpret_cast<char*>(&animation.duration), DURATION_LENGTH);
 
             // Read CIFF data
             animation.ciff_data.parse_image_from_stream(file);
