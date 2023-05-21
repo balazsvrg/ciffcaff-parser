a16e9ac5ec92e1df1038b215aac97edccee9b97f
Balázs Varga <balazs.vrg98@gmail.com>
2023-05-21 12:32:40 +0000

    first working prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
+
 #include "caff.h"
+#include "ciff.h"
+#include "JpegEncoder.h"
 
 int main() {
     // Specify the path to the CAFF file
-    std::string caffFile = "/workspaces/ciffcaff-parser/data/1.caff";
+    std::string caffFile = "/workspaces/ciffcaff-parser/data/3.caff";
 
     // Create an instance of the CAFF class
     CAFF caff(caffFile);
 
     // Parse the CAFF image
     if (caff.parseImage()) {
-        // Get the CAFF header
-        const CaffHeader& header = caff.getHeader();
+        
+        CIFF image = caff.getAnimations().front().ciffData;
+        CiffHeader header = image.get_header();
+        JpegEncoder encoder("out.jpeg", header.width, header.height, 100);
+        encoder.encode(image.get_data());
 
         // Output the header information
-        std::cout << "CAFF Header:" << std::endl;
-        std::cout << "Magic: " << header.magic << std::endl;
-        std::cout << "Header Size: " << header.headerSize << std::endl;
-        std::cout << "Number of Animations: " << header.numAnim << std::endl;
+
     }
 
     return 0;
